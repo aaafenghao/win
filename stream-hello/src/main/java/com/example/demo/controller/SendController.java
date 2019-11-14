b658222d1a65bfe1d8c56b1dab837c502a26f55b
fenghao <fenghao@ysstech.com>
2019-11-14 18:06:47 +0800

    stream 测试类

--- stream-hello/src/main/java/com/example/demo/controller/SendController.java
+++ stream-hello/src/main/java/com/example/demo/controller/SendController.java
@@ -1,31 +1,37 @@
 package com.example.demo.controller;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.cloud.stream.annotation.EnableBinding;
-import org.springframework.cloud.stream.annotation.Output;
-import org.springframework.messaging.MessageChannel;
-import org.springframework.messaging.support.MessageBuilder;
+
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.example.demo.entity.TestVo;
 
 
 @RestController
-@EnableBinding(SendController.SinkSender.class)
+//@EnableBinding(SendController.SinkSender.class)
 public class SendController {
 	
-	@Autowired
-	private SinkSender sender;
+//	@Autowired
+//	private SinkSender sender;
 
-	@GetMapping()
-	public String send() {
-		sender.output().send(MessageBuilder.withPayload("send Test").build());
+	
+	
+	@GetMapping("json")
+	public String send(@RequestBody TestVo vo) {
+//		sender.output().send(MessageBuilder.withPayload("send Test").build());
+		System.out.println(vo.toString());
+		StackTraceElement[] stackTrace = Thread.currentThread().getStackTrace();
+		for (int i = 0; i < stackTrace.length; i++) {
+			System.out.println(stackTrace[i].toString());
+		}
 		return "success";
 	}
 	
-	public interface SinkSender{
-		String OUT_PUT = "output";
-		
-		@Output(SinkSender.OUT_PUT)
-		MessageChannel output();
-	}
+//	public interface SinkSender{
+//		String OUT_PUT = "output";
+//		
+//		@Output(SinkSender.OUT_PUT)
+//		MessageChannel output();
+//	}
 }
--- stream-hello/src/main/java/com/example/demo/service/SinkReceiver.java
+++ stream-hello/src/main/java/com/example/demo/service/SinkReceiver.java
@@ -14,17 +14,17 @@ import org.springframework.cloud.stream.messaging.Sink;
  * @author pc
  *
  */
-@EnableBinding(Sink.class)
+//@EnableBinding(Sink.class)
 public class SinkReceiver {
 	
 	private final static Logger logger = LoggerFactory.getLogger(SinkReceiver.class);
-
-	@StreamListener(Sink.INPUT)
-	public void receiver(Object payload) {
-		logger.info("receiver:"+payload); 
-		
-		List<String> list = new ArrayList<String>();
-		
-		
-	}
+//
+//	@StreamListener(Sink.INPUT)
+//	public void receiver(Object payload) {
+//		logger.info("receiver:"+payload); 
+//		
+//		List<String> list = new ArrayList<String>();
+//		
+//		
+//	}
 }
